3ee7fd55ecdb3b54d1175b6aa46787f6c3ced1fa
rodrigo <rodrigo@gato>
2019-09-20 22:20:38 -0300

    fixed bug

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ import scrapy
 logger = logging.getLogger()
 
 URL_BASE = 'https://www.portalinmobiliario.com/venta/casa/'
-URL_SUFFIX = '?ca=3&ts=1&mn=2&or=&sf=1&sp=0&at=0&pg=1'
+URL_SUFFIX = '?ca=3&ts=1&mn=2&or=&sf=1&sp=0&at=0&pg=76'
 
 URL_LAS_CONDES = URL_BASE + 'las-condes-metropolitana' + URL_SUFFIX
 URL_NUNOA = URL_BASE + 'nunoa-metropolitana' + URL_SUFFIX
@@ -21,22 +21,25 @@ class BuildingsSpider(scrapy.Spider):
         town = response.css('.results-title').xpath('.//ol/li[5]/text()').get()
         for item in response.css('.propiedad'):
             item_data = item.css('.product-item-data')
-
-            item_area = item_data.xpath('.//div/div[3]/p/span/@data-title').get().replace('m&sup2;', '').split('/')
-            data_1 = item_area[0].strip().split(' ')
-            if len(item_area) > 1:
-                data_2 = item_area[1].strip().split(' ')
+            item_area = item_data.xpath('.//div/div[3]/p/span/@data-title').get()
 
             area_building = -1
             area_terrain = -1
-            if data_1[1].lower() == 'construida':
-                area_building = data_1[0]
-            if data_2[1].lower() == 'construida':
-                area_building = data_2[0]
-            if data_1[1].lower() == 'terreno':
-                area_terrain = data_1[0]
-            if data_2[1].lower() == 'terreno':
-                area_terrain = data_2[0]
+            if item_area is not None:
+                item_area = item_area.replace('m&sup2;', '').split('/')
+
+                data_1 = item_area[0].strip().replace('.', '').split(' ')
+                if len(item_area) > 1:
+                    data_2 = item_area[1].strip().replace('.', '').split(' ')
+
+                if data_1[1].lower() == 'construida':
+                    area_building = data_1[0]
+                if data_2[1].lower() == 'construida':
+                    area_building = data_2[0]
+                if data_1[1].lower() == 'terreno':
+                    area_terrain = data_1[0]
+                if data_2[1].lower() == 'terreno':
+                    area_terrain = data_2[0]
 
             yield {
                 'town': town,
