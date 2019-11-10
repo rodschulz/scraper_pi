1189c96574ce881a3f577c931fa126e7669c3f0d
Rodrigo <rodrigo@gato>
2019-11-10 11:16:04 +0100

    Minor changes in plotter

--- plotter.py
+++ plotter.py
@@ -6,7 +6,12 @@ from logging import config, getLogger
 config.fileConfig('log.ini', disable_existing_loggers=False)
 logger = getLogger()
 
+_TO_PESO = 28500.0
+_TO_UF = 1.0 / _TO_PESO
+
 _DATA_ROOT = 'data'
+_DATA_CURR_PESOS = 1
+_DATA_CURR_UF = 2
 
 if __name__ == '__main__':
     logger.info(' >> START!')
@@ -16,9 +21,15 @@ if __name__ == '__main__':
         filename = os.fsdecode(file)
         logger.info('opening "%s"', filename)
 
+        price = {
+            'mean': 0,
+            'max': -1,
+            'min': 1E10,
+        }
         with open('./data/' + filename, 'r') as json_file:
             data = json.load(json_file)
             for d in data[_DATA_ROOT]:
                 logger.info(d)
+                break
 
     logger.info(' >> FINISHED!')
