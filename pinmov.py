3c9efb61ed6093813594b4bf914f2ce6c72b2c13
rodrigo <rodrigo@gato>
2020-05-03 16:12:46 +0200

    reworked scritp

--- pinmov.py
+++ pinmov.py
@@ -1,5 +1,5 @@
-import logging
 import logging.config
+# import logging.getLogger
 import numbers
 
 import scrapy
@@ -15,20 +15,33 @@ _URL_NUNOA = 'https://www.portalinmobiliario.com/venta/casa/nunoa-metropolitana'
 
 _SCRAP_TYPE = ['REG', 'PRO']
 
-# TODO
-# - UT link formats different (old urls) => https://www.portalinmobiliario.com/MLC-517628020-casa-en-venta-de-4-dormitorios-en-las-condes-_JM#position=1&type=item&tracking_id=e2af56e6-6952-44fb-a843-df1f5f0d94c3
-# - UT for missing 'attrs'
-# - UT for missing id
-# - UT for missing spec items (sup total, sup util, dorms, banos)
+
+class ScrapUtils(object):
+    """
+    Utils class to ease the scrapping
+    """
+    @staticmethod
+    def parse_area(surface):
+        if surface is None or type(surface) is not str:
+            return -1
+        else:
+            return int(surface.split(' ')[0])
+
+    @staticmethod
+    def parse_rooms(number):
+        if number is None or type(number) is not str:
+            return -1
+        else:
+            return int(number)
 
 
 class BuildingsSpider(scrapy.Spider):
     name = "buildings"
     start_urls = [
         # _URL_LAS_CONDES,
-        # _URL_PROVIDENCIA,
+        _URL_PROVIDENCIA,
         # _URL_LA_REINA,
-        _URL_NUNOA,
+        # _URL_NUNOA,
     ]
 
     custom_settings = {
@@ -50,129 +63,76 @@ class BuildingsSpider(scrapy.Spider):
 
         scrap_id = 0
         for item in items:
-            is_project = item.css('.item__image-label::text').get() is not None
-            currency = item.css('.price__symbol::text').get()
-            price = float(item.css('.price__fraction::text').get().replace('.', ''))
-            attrs = item.css('.item__attrs::text').extract()[0].replace('\xa0', ' ')
-            brief = '|'.join(item.css('.main-title::text').getall())
-            link = item.css('.item__info-link::attr(href)').get().split('#')[0]
-            town = response.url.strip('/').split('/')[-1]
-            # item_id = tmp[6].split('-')[0]
-
             scrap_id += 1
 
-            data = {
-                'scrap_id': scrap_id,
-                'is_project': is_project,
-                'town': town,
-                # 'id': item_id,
-                'price': price,
-                'currency': currency,
-                'link': link,
-                'brief': brief,
-                'attrs': attrs,
-                'terrain': -1,
-                'building': -1,
-                'bedrooms': -1,
-                'bathrooms': -1,
-            }
+            try:
+                is_project = item.css('.item__image-label::text').get() is not None
+                currency = item.css('.price__symbol::text').get()
+                price = float(item.css('.price__fraction::text').get().replace('.', ''))
+                attrs = item.css('.item__attrs::text').extract()[0].replace('\xa0', ' ')
+                brief = '|'.join(item.css('.main-title::text').getall())
+                link = item.css('.item__info-link::attr(href)').get().split('#')[0]
+                town = response.url.strip('/').split('/')[-1]
 
-            logger.debug(' > scrap {:02}: {}...'.format(scrap_id, data['link'][34:34+min(70, len(data['link']))]))
-            yield scrapy.Request(link, callback=BuildingsSpider.parse_details, cb_kwargs={'data': data})
+                tmp = link.split('/')
+                if len(tmp) == 4:   # old url format
+                    item_id = tmp[3].split('-')[1]
+                else:               # new url format
+                    item_id = tmp[6].split('-')[0]
 
-            # if scrap_id == 10:
-            #     break
+                data = {
+                    'scrap_id': scrap_id,
+                    'is_project': is_project,
+                    'town': town,
+                    'id': item_id,
+                    'price': price,
+                    'currency': currency,
+                    'link': link,
+                    'brief': brief,
+                    'attrs': attrs,
+                    'terrain': -1,
+                    'building': -1,
+                    'bedrooms': -1,
+                    'bathrooms': -1,
+                }
+
+                logger.debug(' > scrap {:02}: {}...'.format(scrap_id, data['link'][34:34 + min(70, len(data['link']))]))
+                yield scrapy.Request(link,
+                                     callback=BuildingsSpider.parse_details,
+                                     cb_kwargs={'data': data},
+                                     meta={'dont_redirect': True,'handle_httpstatus_list': [302]}
+                                     )
+
+            except Exception as ex:
+                logger.error('\n\tscrap_id:{:02}\n\turl: {}\n\tlink: {}'.format(
+                            scrap_id, response.url, link))
+                logger.error('Exception: {}'.format(str(ex)))
 
         # parse next page of results
-        # next_page = response.css('.andes-pagination__button--next a::attr(href)').get()
-        # if next_page is not None:
-        #     yield response.follow(next_page, callback=self.parse)
+        next_page = response.css('.andes-pagination__button--next a::attr(href)').get()
+        if next_page is not None:
+            yield scrapy.Request(next_page, callback=self.parse)
 
     @staticmethod
     def parse_details(response, data):
         logger.debug('  ({}) scrap: {:03}'.format(_SCRAP_TYPE[data['is_project']], data['scrap_id']))
 
-        if data['is_project']:
-            valid, details = BuildingsSpider.details_project(response, data)
-        else:
-            valid, details = BuildingsSpider.details_regular(response, data)
+        try:
+            terrain = ScrapUtils.parse_area(response.xpath('//strong[contains(.,"total")]/following-sibling::span/text()').get())
+            building = ScrapUtils.parse_area(response.xpath('//strong[contains(.,"útil")]/following-sibling::span/text()').get())
+            bedrooms = ScrapUtils.parse_rooms(response.xpath('//strong[contains(.,"Dormitorios")]/following-sibling::span/text()').get())
+            bathrooms = ScrapUtils.parse_rooms(response.xpath('//strong[contains(.,"Baños")]/following-sibling::span/text()').get())
 
-        if valid:
-            data.update(details)
+            data.update({
+                'terrain': terrain,
+                'building': building,
+                'bedrooms': bedrooms,
+                'bathrooms': bathrooms
+            })
+
+        except Exception as ex:
+            logger.error('\n\tscrap_id:{:02}\n\ttype: {}\n\turl: {}'.format(
+                data['scrap_id'], _SCRAP_TYPE[data['is_project']], response.url))
+            logger.error('Exception: {}'.format(str(ex)))
 
         yield data
-
-    @staticmethod
-    def details_regular(response, data):
-
-
-        # terrain = response. \
-        #     xpath('//*[@id="root-app"]/div/div[1]/div[1]/section[1]/div/div/div/section/ul/li[1]/span/text()').get()
-        # terrain = terrain.split(' ')[0]
-        # if terrain.isdigit():
-        #     terrain = int(terrain)
-        #
-        # building = response. \
-        #     xpath('//*[@id="root-app"]/div/div[1]/div[1]/section[1]/div/div/div/section/ul/li[2]/span/text()').get()
-        # building = building.split(' ')[0]
-        # if building.isdigit():
-        #     building = int(building)
-        #
-        # bedrooms = response. \
-        #     xpath('//*[@id="root-app"]/div/div[1]/div[1]/section[1]/div/div/div/section/ul/li[3]/span/text()').get()
-        # if bedrooms.isdigit():
-        #     bedrooms = int(bedrooms)
-        #
-        # bathrooms = response. \
-        #     xpath('//*[@id="root-app"]/div/div[1]/div[1]/section[1]/div/div/div/section/ul/li[4]/span/text()').get()
-        # if bathrooms.isdigit():
-        #     bathrooms = int(bathrooms)
-        #
-        # is_valid = isinstance(terrain, numbers.Number) \
-        #            and isinstance(building, numbers.Number) \
-        #            and isinstance(bedrooms, numbers.Number) \
-        #            and isinstance(bathrooms, numbers.Number)
-        # data = {
-        #     'terrain': terrain,
-        #     'building': building,
-        #     'bedrooms': bedrooms,
-        #     'bathrooms': bathrooms
-        # }
-        #
-        # return is_valid, data
-
-        return True, {'terrain': 10, 'building': 100}
-
-
-    @staticmethod
-    def details_project(response, data):
-        # building = response. \
-        #     xpath('//*[@id="root-app"]/div[2]/div[1]/div[1]/section[2]/div[1]/section/ul/li[1]/span/text()').get()
-        # try:
-        #     building = building.split(' ')[0]
-        #     building = int(float(building))
-        # except Exception:
-        #     pass
-        #
-        # bedrooms = response. \
-        #     xpath('//*[@id="root-app"]/div[2]/div[1]/div[1]/section[2]/div[1]/section/ul/li[2]/span/text()').get()
-        # if bedrooms.isdigit():
-        #     bedrooms = int(bedrooms)
-        #
-        # bathrooms = response. \
-        #     xpath('//*[@id="root-app"]/div[2]/div[1]/div[1]/section[2]/div[1]/section/ul/li[3]/span/text()').get()
-        # if bathrooms.isdigit():
-        #     bathrooms = int(bathrooms)
-        #
-        # is_valid = isinstance(building, numbers.Number) \
-        #            and isinstance(bedrooms, numbers.Number) \
-        #            and isinstance(bathrooms, numbers.Number)
-        # data = {
-        #     'building': building,
-        #     'bedrooms': bedrooms,
-        #     'bathrooms': bathrooms
-        # }
-        #
-        # return is_valid, data
-
-        return True, {'terrain': 10, 'building': 100}
